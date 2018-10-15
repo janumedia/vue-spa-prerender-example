dfc41d3a277499b215fca10ff8e07cd57981aa63
janumedia@yahoo.com.sg <janumedia@gmail.com>
2018-10-16 00:17:09 +0800

    disableHostCheck

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-    productionSourceMap: false
+    productionSourceMap: false,
+    //Invalid Host Header issue
+    //https://github.com/vuejs-templates/webpack/issues/1205#issuecomment-404321666
+    devServer: {
+        disableHostCheck: true
+    }
 }
