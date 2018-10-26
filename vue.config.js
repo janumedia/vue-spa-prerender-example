13856c1afc0c2a410e4d8d121154404abe7ab35c
janumedia@yahoo.com.sg <janumedia@gmail.com>
2018-10-26 14:00:33 +0800

    add preprender-spa

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+let path = require('path');
 module.exports = {
+    configureWebpack: config => {
+        //if(process.env.NODE_ENV !== 'production') return;
+        return {
+            plugins: [
+                new PrerenderSPAPlugin({
+                    staticDir: path.join(__dirname, 'dist'),
+                    routes: ['/', '/tiles-grid', '/chess-grid', '/list-box', '/google', '/404'],
+                    renderer: new PrerenderSPAPlugin.PuppeteerRenderer({
+                        renderAfterTime: 5000
+                    })
+                })
+            ]
+        }
+    },
     productionSourceMap: false,
     //Invalid Host Header issue
     //https://github.com/vuejs-templates/webpack/issues/1205#issuecomment-404321666
